201c10056e153539aaee559995fc0d1d1c4a592c
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2026-03-19 11:15:35 +0800

    T7_S3_JCLS_LITENOTES

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//add this one
+use App\Http\Controllers\NoteController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Added Codes
+
+Route::resource('/notes', NoteController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
