4d9b4943b72db964298c3147238f68b84e97d385
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2026-03-30 08:26:13 +0800

    LAST_S7_JCLS_LITENOTES

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Notes') }}
+            {{ !$note->trashed() ? __('Notes') : __('Trash') }}
         </h2>
     </x-slot>
 
@@ -11,20 +11,36 @@
                 {{ session('success') }}
             </x-alert-success>
             <div class="flex">
-                <p class="opacity-70">
-                    <strong>Created: </strong> {{ $note->created_at->diffForHumans() }}
-                </p>
-                <p class="opacity-70 ml-4">
-                    <strong>Updated at: </strong> {{ $note->updated_at->diffForHumans() }}
-                </p>
-                <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">Edit Note</a>
+                @if(!$note->trashed())
+                    <p class="opacity-70">
+                        <strong>Created: </strong> {{ $note->created_at->diffForHumans() }}
+                    </p>
+                    <p class="opacity-70 ml-4">
+                        <strong>Updated: </strong> {{ $note->updated_at->diffForHumans() }}
+                    </p>
+                    <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">Edit Note</a>
 
-                <form action="{{ route('notes.destroy', $note) }}" method="post">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you wish to delete this note?')">Move to Trash</button>
-                </form>
+                    <form action="{{ route('notes.destroy', $note) }}" method="post">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you wish to move this to trash?')">Move to Trash</button>
+                    </form>
+                @else
+                    <p class="opacity-70">
+                        <strong>Delete: </strong> {{ $note->deleted_at->diffForHumans() }}
+                    </p>
 
+                    <form action="{{ route('trashed.update', $note) }}" method="post" class="ml-auto">
+                        @method('put')
+                        @csrf
+                        <button type="submit" class="btn-link">Restore Note</button>
+                    </form>
+                    <form action="{{ route('trashed.destroy', $note) }}" method="post">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you wish to delete this note forever? This action cannot be undone')">Delete Forever</button>
+                    </form>
+                @endif
             </div>
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 <h2 class="font-bold text-2xl">
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,12 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('/notes', NoteController::class)->middleware(['auth']);
 
-Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
+Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function() {
+    Route::get('/', [TrashedNoteController::class, 'index'])->name('index');
+    Route::get('/{note}', [TrashedNoteController::class, 'show'])->name('show')->withTrashed();
+    Route::put('/{note}', [TrashedNoteController::class, 'update'])->name('update')->withTrashed();
+    Route::delete('/{note}', [TrashedNoteController::class, 'destroy'])->name('destroy')->withTrashed();
+
+});
 
 require __DIR__.'/auth.php';
